5468793fc8d8f618618e341468698191a3e29bb5
feiqian <710331184@qq.com>
2016-08-02 12:43:58 +0800

    add loading animate

--- source/js/index.js
+++ source/js/index.js
@@ -1,26 +1,43 @@
-jQuery(function(){
+$(function(){
+    var isHome = window.location.pathname == "/" && window.location.hash == "";
+
+    //显示加载动画
+    function  addLoading() {
+        if(isHome){
+            var loadingEle = $("div.overlay:first");
+            loadingEle.show();
+
+            $(window).on('load',function () {
+                //移除loading动画
+                (function removeLoading() {
+                    loadingEle.slideUp();
+                    loadingEle = null;
+                })();
+            });
+        }
+    }
+
+    //显示封面
     function showConver()
     {
-        var isHome = window.location.pathname == "/" && window.location.hash == "";
-
-        var index_conver =jQuery("div.panel-cover:first");
-        var blog_conver =jQuery("div.body_container:first");
+        var index_conver =$("div.panel-cover:first");
+        var blog_conver =$("div.body_container:first");
 
         if(isHome)
         {
             index_conver.fadeIn();
 
-            jQuery("a.blog-button").click(function(){
-                index_conver.fadeOut();
+            index_conver.find("a.blog-button").click(function(){
+                index_conver.slideUp();
                 blog_conver.show();
             });
         }
         else
         {
-            blog_conver.fadeIn();
+            blog_conver.show();
         }
     }
 
+    addLoading();
     showConver();
-
 });
